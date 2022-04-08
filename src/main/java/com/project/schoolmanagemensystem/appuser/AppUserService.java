e2938fa8965cbc08a02fbe9d66a63e228dc8a11e
Adeybabs <37551521+adeybabs@users.noreply.github.com>
2022-04-08 16:00:43 +0100

    mail token confirmation added

--- src/main/java/com/project/schoolmanagemensystem/appuser/AppUserService.java
+++ src/main/java/com/project/schoolmanagemensystem/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.project.schoolmanagemensystem.appuser;
 
+import com.project.schoolmanagemensystem.registration.token.ConfirmationToken;
+import com.project.schoolmanagemensystem.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +21,7 @@ public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -38,8 +44,23 @@ public class AppUserService implements UserDetailsService {
 
        appUserRepository.save(appUser);
 
-       //TODO: Send confirmation token
 
-        return "it works";
+        //create a token and save it using the method below
+        String token = UUID.randomUUID().toString();
+        ConfirmationToken confirmationtoken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationtoken);
+
+        //TODO: SEND EMAIL
+        return token;
     }
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
+    }
+
 }
---
+++ src/main/java/com/project/schoolmanagemensystem/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.project.schoolmanagemensystem.email;
+
+public interface EmailSender {
+    void send(String to, String email );
+}
